028903903dd620537f6f4c017194a2be6aceb240
Mazen Kamel <107046923+mazenwk@users.noreply.github.com>
2024-10-24 17:35:19 +0300

    cleanup

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     """
     # Determine the path to the processing script
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    processing_script = os.path.join(script_dir, 'pedestrians-pcds-cropping', 'process_pedestrians.py')
+    processing_script = os.path.join(script_dir, 'LokiPedCrop', 'process_pedestrians.py')
 
     if not os.path.exists(processing_script):
         print(f"Processing script not found at {processing_script}")
@@ -16,9 +16,8 @@ def main():
 
     # Pass all command-line arguments to the processing script
     args = [
-        # ['--root_dir', r'./pedestrians-pcds-cropping/sample/'],
         ['--root_dir', r'./LOKI/'],
-        ['--csv_path', r'./pedestrians-pcds-cropping/loki.csv'],
+        ['--csv_path', r'./LokiPedCrop/loki.csv'],
         ['--threshold_multiplier', 0.3]
     ]
     try:
